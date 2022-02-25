37e9e3f2043917d30b90f631d34bbd5ad49ebef8
Javier Alejandro Ramírez Heredia <ram21600@uvg.edu.gt>
2022-02-25 17:04:50 -0600

    BubbleSort
    Creación del metodo BubleSort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,20 @@
 public class BubbleSort{
-    
+
+    public int[] ordenar(int[] arreglo) {
+
+        int n = arreglo.length;  
+        int temp = 0; 
+         
+        for(int i=0; i < n; i++){  
+            for(int j=1; j < (n-i); j++){  
+                if(arreglo[j-1] > arreglo[j]){  
+                    temp = arreglo[j-1];  
+                    arreglo[j-1] = arreglo[j];  
+                    arreglo[j] = temp;  
+                }             
+            }  
+        } 
+
+        return arreglo; 
+    }
 }
